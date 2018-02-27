6535ee45b965b14ae7df774878265ccc186e6799
hagankristen <kristenhagan@DESKTOP-F6L9VU9.localdomain>
2018-02-27 11:12:40 -0500

    check to interpolate

--- ProcessECG.py
+++ ProcessECG.py
@@ -2,8 +2,8 @@ def main():
     try:
         import numpy as np
         import os
-        from ReadCsvECG import GetCsvData
-        from PatientHeartRate import PatientHeartInfo
+        from ReadECG import GetData
+        from HeartRate import PatientInfo
         import logging as lg
         import scipy
 
@@ -12,10 +12,10 @@ def main():
         lg.debug(' | ABORTED: ImportError: %s' % e.name)
 
     cwd = os.getcwd()
-    csv_path = os.path.join(cwd, 'test_data/test_data1.csv')
+    csv_path = os.path.join(cwd, 'test_data/test_data9.csv')
 
     try:
-        patient = GetCsvData(csv_path)
+        patient = GetData(csv_path)
         print('Success: ECG Data extracted from csv.')
     except TypeError:
         print('Input file not csv format or does not exist.')
@@ -24,20 +24,29 @@ def main():
     except IOError:
         print('Empty input file.')
 
+    flag = CheckData(patient)
+    if flag:
+        patient = InterpolateData(patient)
+
     try:
-        patient_heart_info = PatientHeartInfo(patient)
+        patient_heart_info = PatientInfo(patient)
         print(patient_heart_info.calc_num_beats())
         print(patient_heart_info.calc_bpm())
+        print(len(patient_heart_info.calc_beat_times()))
+
     except:
         print('not done')
-    # import function files
 
-    # try
-    # define class of patientheartrateinfo that has attribues given
 
-    # create data classes inside the patientheartinfo class
-    # except print out reason for error (dont raise again)
-    #
+def CheckData(patient):
+    import numpy as np
+    flag = 0
+    flag = np.isnan(patient.data).any()
+    if flag:
+        print('Imported data requires interpolation.')
+    else:
+        print('Imported data does not require interpolation.')
+    return flag
 
 
 if __name__ == "__main__":
---
+++ ReadECG.py
@@ -0,0 +1,19 @@
+class GetData:
+    def __init__(self, csv_file):
+        import os
+        import numpy as np
+        import warnings
+
+        extension = os.path.splitext(csv_file)[1]
+        if extension != '.csv':
+            raise TypeError
+        flag = os.path.exists(csv_file)
+        if flag is False:
+            raise TypeError
+        self.path_name = csv_file
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            self.data = np.genfromtxt(
+                csv_file, delimiter=",", dtype=(float, float))
+        if len(self.data) == 0:
+            raise IOError
