daacb8e2ded89f6ef485125ddacc6a00addb5b9f
hagankristen <kristenhagan@DESKTOP-F6L9VU9.localdomain>
2018-02-28 11:57:25 -0500

    more tests

--- test_PatientHeartRate.py
+++ test_PatientHeartRate.py
@@ -8,15 +8,14 @@ import logging as lg
 import warnings
 
 
-# def test_calc_volt_ex():
-#    patient1 = PatientInfo('test_data/test_data1.csv')
-#    print(patient1.voltage_extremes)
+def test_calc_volt_ex():
+    patient1 = PatientInfo('test_data/test_data1.csv')
+    assert(patient1.voltage_extremes == (-0.68, 1.05))
 
 def test_check_volt_range():
     with pytest.raises(ValueError):
         PatientInfo(GetData('test_data/test_data32.csv'))
 
-
-# def test_calc_duration():
-#    patient2 = PatientInfo('test_data/test_data1.csv')
-#    print(patient2.duration)
+def test_calc_duration():
+    patient2 = PatientInfo('test_data/test_data1.csv')
+    assert(np.isclose(patient2.duration, 27.775))
