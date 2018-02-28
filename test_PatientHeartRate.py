82ee2c733c1ccb1d6f1284bc283b89849874566d
hagankristen <kristenhagan@DESKTOP-F6L9VU9.localdomain>
2018-02-28 00:08:56 -0500

    first test fxn

---
+++ test_PatientHeartRate.py
@@ -0,0 +1,13 @@
+import pytest
+from PatientHeartRate import PatientInfo
+from ReadECG import GetData
+import os
+import numpy as np
+import scipy.signal as sig
+import logging as lg
+
+def test_GetData():
+    with pytest.raises(TypeError):
+        GetData('testdata.json')
+    with pytest.raises(ImportError):
+        GetData('notreal.csv')
