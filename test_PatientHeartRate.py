87d1ab0e640fc542a2ca4b8b1d213df287077cb8
hagankristen <kristenhagan@DESKTOP-F6L9VU9.localdomain>
2018-02-28 00:36:06 -0500

    try agani

--- test_PatientHeartRate.py
+++ test_PatientHeartRate.py
@@ -6,28 +6,31 @@ import numpy as np
 import scipy.signal as sig
 import logging as lg
 
+
 def test_GetData():
     with pytest.raises(TypeError):
         GetData('testdata.json')
     with pytest.raises(ImportError):
         GetData('notreal.csv')
 
+
 def test_load_ecg():
     with pytest.raises(TypeError):
-        GetData('testdata.json')
+        PatientInfo('testdata.json')
     with pytest.raises(ImportError):
-        GetData('notreal.csv')
+        PatientInfo('notreal.csv')
+
 
 def test_calc_volt_ex():
     patient = PatientInfo('test_data/test_data1.csv')
     assert patient.voltage_extremes == (-.68, 1.05)
 
+
 def test_check_volt_range():
     with pytest.raises(ValueError):
         PatientInfo('test_data/test_data32.csv')
 
+
 def test_calc_duration():
     patient = PatientInfo('test_data/test_data1.csv')
     assert(np.isclose(patient.duration, 27.775))
-
-    
